8796b3e12b919d53366319ebc8ec3c0b11396cf8
Samra-ca <sminhas1405@gmail.com>
2026-06-20 15:07:28 +0500

    Update Firebase config for Hugging Face

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,11 +1,14 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const db   = admin.firestore();
+const db = admin.firestore();
 const auth = admin.auth();
 
 module.exports = { admin, db, auth };
