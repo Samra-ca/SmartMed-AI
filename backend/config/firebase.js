8058b91c039abb7b26ccc55f8497eaee6d74af5e
Samra-ca <sminhas1405@gmail.com>
2026-06-20 14:12:17 +0500

    Backend Correct

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,14 +1,11 @@
 const admin = require('firebase-admin');
-
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
-);
+const serviceAccount = require('../serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const db = admin.firestore();
+const db   = admin.firestore();
 const auth = admin.auth();
 
 module.exports = { admin, db, auth };
